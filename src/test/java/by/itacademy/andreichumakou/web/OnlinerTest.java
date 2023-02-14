f9375e72fc1024aa09418f8490593f5da5086ceb
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 14:33:41 +0300

    refactor OnlinerTest adding testOnlinerLoginFormWithEmptyPassword method

--- src/test/java/by/itacademy/andreichumakou/web/OnlinerTest.java
+++ src/test/java/by/itacademy/andreichumakou/web/OnlinerTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class OnlinerTest {
@@ -29,8 +33,33 @@ public class OnlinerTest {
         driver.get(OnlinerMainPage.URL);
         driver.findElement(By.xpath(OnlinerMainPage.ENTER_XPATH)).click();
 
-        String actualTitleAuthForm = driver.findElement(By.xpath(OnlinerEnterPage.TITLE_AUTH_FORM_XPATH)).getText();
+        String actualTitleAuthForm = driver.findElement(By.xpath(OnlinerEnterPage
+                .TITLE_AUTH_FORM_XPATH)).getText();
         Assert.assertEquals(OnlinerEnterPage.TITLE_AUTH_FORM_TEXT, actualTitleAuthForm);
         driver.quit();
     }
+
+    @Test
+    public void testOnlinerLoginFormWithEmptyPassword() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(OnlinerMainPage.URL);
+        driver.findElement(By.xpath(OnlinerMainPage.ENTER_XPATH)).click();
+        driver.findElement(By.xpath(OnlinerEnterPage.BUTTON_ENTER_XPATH)).click();
+
+        new WebDriverWait(driver, Duration.ofMillis(10000)).until(ExpectedConditions
+                .presenceOfElementLocated(By.xpath(OnlinerEnterPage
+                        .ERROR_MSG_AUTH_FORM_NAME_XPATH)));
+
+        String actualMessageErrorName = driver.findElement(By.xpath(OnlinerEnterPage
+                .ERROR_MSG_AUTH_FORM_NAME_XPATH)).getText();
+        String actualMessageErrorPassword = driver.findElement(By.xpath(OnlinerEnterPage
+                .ERROR_MSG_AUTH_FORM_PASSWORD_XPATH)).getText();
+        Assert.assertEquals(OnlinerEnterPage.ERROR_MSG_AUTH_FORM_NAME_TEXT, actualMessageErrorName);
+        Assert.assertEquals(OnlinerEnterPage.ERROR_MSG_AUTH_FORM_PASSWORD_TEXT, actualMessageErrorPassword);
+        driver.quit();
+    }
+
+
 }
