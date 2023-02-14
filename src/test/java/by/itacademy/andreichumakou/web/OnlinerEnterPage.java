c95f41db892b3b585b895cd1b74769be773f0cd8
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 12:13:43 +0300

    add OnlinerEnterPage.java data

---
+++ src/test/java/by/itacademy/andreichumakou/web/OnlinerEnterPage.java
@@ -0,0 +1,6 @@
+package by.itacademy.andreichumakou.web;
+
+public class OnlinerEnterPage {
+    public static final String TITLE_AUTH_FORM_XPATH = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String TITLE_AUTH_FORM_TEXT = "Вход";
+}
