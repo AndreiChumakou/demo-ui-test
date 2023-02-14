f3d0fba16ca1ef780bfad2aadc4f0c44b610152c
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 18:48:04 +0300

    refactor AmazonePage adding data

--- src/test/java/by/itacademy/andreichumakou/web/AmazonPage.java
+++ src/test/java/by/itacademy/andreichumakou/web/AmazonPage.java
@@ -5,6 +5,16 @@ public class AmazonPage {
     public static final String COPYRIGHT_XPATH = "//span[contains(text(), '© 1996-2023, Amazon')]";
     public static final String COPYRIGHT_TEXT = "© 1996-2023, Amazon.com, Inc. or its affiliates";
 
+    public static final String CART_FROM_MAIN_XPATH = "//span[contains(text(),'Cart')]";
+    public static final String ROW_YOUR_AMAZON_CART_IS_EMPTY_XPATH =
+            "//div[@class='a-row sc-your-amazon-cart-is-empty']";
+    public static final String ROW_YOUR_AMAZON_CART_IS_EMPTY_TEXT = "Your Amazon Cart is empty";
+
+
+
+
+
+
 
 
 
