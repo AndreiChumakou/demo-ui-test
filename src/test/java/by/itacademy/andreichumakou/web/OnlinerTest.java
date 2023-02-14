d18df998da0d56a7b3c7482cb3a25d1725e9a1f4
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 11:20:22 +0300

    refactor OnlinerTest add testOpenOnliner method

--- src/test/java/by/itacademy/andreichumakou/web/OnlinerTest.java
+++ src/test/java/by/itacademy/andreichumakou/web/OnlinerTest.java
@@ -1,4 +1,23 @@
 package by.itacademy.andreichumakou.web;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class OnlinerTest {
+
+    @Test
+    public void testOpenOnliner() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(OnlinerPage.URL);
+        String actualCopyright = driver.findElement(By.xpath(OnlinerPage.COPYRIGHT_XPATH)).getText();
+
+        Assert.assertEquals(actualCopyright, OnlinerPage.COPYRIGHT_TEXT);
+        driver.quit();
+    }
 }
