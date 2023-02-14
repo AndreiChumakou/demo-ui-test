dc5a0c1970f00c8bd622b5db9c6d3904fd1deb6f
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 12:14:39 +0300

    refactor OnlinerMainPage add enterPage data

--- src/test/java/by/itacademy/andreichumakou/web/OnlinerMainPage.java
+++ src/test/java/by/itacademy/andreichumakou/web/OnlinerMainPage.java
@@ -1,8 +1,10 @@
 package by.itacademy.andreichumakou.web;
 
-public class OnlinerPage {
+public class OnlinerMainPage {
     public static final String URL = "https://www.onliner.by/";
     public static final String COPYRIGHT_XPATH = "//div[@class='footer-style__copy']";
     public static final String COPYRIGHT_TEXT = "© 2001—2023 Onlíner";
+    public static final String ENTER_XPATH = "//div[@class='auth-bar__item auth-bar__item--text']";
+
 
 }
