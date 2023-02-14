5f7934885f6e0c877977f3d641e82de4f7e59431
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 14:32:52 +0300

    refactor OnlinerEnterPage adding data

--- src/test/java/by/itacademy/andreichumakou/web/OnlinerEnterPage.java
+++ src/test/java/by/itacademy/andreichumakou/web/OnlinerEnterPage.java
@@ -3,4 +3,12 @@ package by.itacademy.andreichumakou.web;
 public class OnlinerEnterPage {
     public static final String TITLE_AUTH_FORM_XPATH = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
     public static final String TITLE_AUTH_FORM_TEXT = "Вход";
+    public static final String BUTTON_ENTER_XPATH = "//button[contains(@class, 'auth-button')]";
+    public static final String ERROR_MSG_AUTH_FORM_NAME_XPATH = "//div[contains(text(), 'Укажите ник или e-mail')]";
+    public static final String ERROR_MSG_AUTH_FORM_NAME_TEXT = "Укажите ник или e-mail";
+    public static final String ERROR_MSG_AUTH_FORM_PASSWORD_XPATH = "//div[contains(text(), 'Укажите пароль')]";
+    public static final String ERROR_MSG_AUTH_FORM_PASSWORD_TEXT = "Укажите пароль";
+
+
+
 }
