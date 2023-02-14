183f058af8678df8d5f5d40ae88546efb1b8994c
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 18:21:43 +0300

    add AmazonPage

---
+++ src/test/java/by/itacademy/andreichumakou/web/AmazonPage.java
@@ -0,0 +1,11 @@
+package by.itacademy.andreichumakou.web;
+
+public class AmazonPage {
+    public static final String URL = "https://www.amazon.com/";
+    public static final String COPYRIGHT_XPATH = "//span[contains(text(), '© 1996-2023, Amazon')]";
+    public static final String COPYRIGHT_TEXT = "© 1996-2023, Amazon.com, Inc. or its affiliates";
+
+
+
+
+}
