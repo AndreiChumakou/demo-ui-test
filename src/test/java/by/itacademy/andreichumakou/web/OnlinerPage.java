48ce6834a241dd06ffbf874c97dc4a240d9bb4cd
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-14 11:19:55 +0300

    refactor OnlinerPage add test data

--- src/test/java/by/itacademy/andreichumakou/web/OnlinerPage.java
+++ src/test/java/by/itacademy/andreichumakou/web/OnlinerPage.java
@@ -1,5 +1,8 @@
 package by.itacademy.andreichumakou.web;
 
 public class OnlinerPage {
+    public static final String URL = "https://www.onliner.by/";
+    public static final String COPYRIGHT_XPATH = "//div[@class='footer-style__copy']";
+    public static final String COPYRIGHT_TEXT = "© 2001—2023 Onlíner";
 
 }
